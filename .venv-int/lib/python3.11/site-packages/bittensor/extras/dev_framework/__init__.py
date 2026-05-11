4f6c6c6c0d9bd67a2b8cbdf6b13add8255deb8cc
Fred E <7602667+wallscaler@users.noreply.github.com>
2026-05-11 14:07:07 -0400

    chore: untrack stale .venv-int + add other venv dirs to gitignore

--- .venv-int/lib/python3.11/site-packages/bittensor/extras/dev_framework/__init__.py
+++
@@ -1,8 +0,0 @@
-from .calls import *  #  noqa:  F403
-from .subnet import (
-    NETUID as NETUID,
-    TestSubnet as TestSubnet,
-    ACTIVATE_SUBNET as ACTIVATE_SUBNET,
-    REGISTER_SUBNET as REGISTER_SUBNET,
-    REGISTER_NEURON as REGISTER_NEURON,
-)
